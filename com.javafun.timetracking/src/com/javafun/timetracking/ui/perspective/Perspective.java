bbfcd35a661cbd8a64cb28314636114f0651783d
agk <alexander.kunin@gmail.com>
2011-10-31 11:37:48 -0700

    perspective

--- com.javafun.timetracking/src/com/javafun/timetracking/ui/perspective/Perspective.java
+++ com.javafun.timetracking/src/com/javafun/timetracking/ui/perspective/Perspective.java
@@ -1,6 +1,5 @@
 package com.javafun.timetracking.ui.perspective;
 
-
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
@@ -14,15 +13,15 @@ import com.javafun.timetracking.ui.view.View;
  */
 public class Perspective implements IPerspectiveFactory {
 
-	public void createInitialLayout(IPageLayout layout) {
-		String editorArea = layout.getEditorArea();
-		layout.setEditorAreaVisible(false);
-		
-		layout.addStandaloneView(NavigationView.ID,  false, IPageLayout.LEFT, 0.25f, editorArea);
-		IFolderLayout folder = layout.createFolder("messages", IPageLayout.TOP, 0.5f, editorArea);
-		folder.addPlaceholder(View.ID + ":*");
-		folder.addView(View.ID);
-		
-		layout.getViewLayout(NavigationView.ID).setCloseable(false);
-	}
+    public void createInitialLayout(IPageLayout layout) {
+        String editorArea = layout.getEditorArea();
+        layout.setEditorAreaVisible(true);
+
+        layout.addStandaloneView(NavigationView.ID, true, IPageLayout.LEFT, 0.25f, editorArea);
+        IFolderLayout folder = layout.createFolder("messages", IPageLayout.TOP, 0.5f, editorArea);
+        folder.addPlaceholder(View.ID + ":*");
+        folder.addView(View.ID);
+
+        layout.getViewLayout(NavigationView.ID).setCloseable(false);
+    }
 }
