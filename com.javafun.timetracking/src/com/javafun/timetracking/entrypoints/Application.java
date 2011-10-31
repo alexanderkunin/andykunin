b60ed9950d18fd545de7fe75ab63e7b79a0a3471
agk <alexander.kunin@gmail.com>
2011-10-31 11:40:34 -0700

    fetched

--- com.javafun.timetracking/src/com/javafun/timetracking/entrypoints/Application.java
+++ com.javafun.timetracking/src/com/javafun/timetracking/entrypoints/Application.java
@@ -1,21 +1,47 @@
 package com.javafun.timetracking.entrypoints;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.rwt.RWT;
 import org.eclipse.rwt.lifecycle.IEntryPoint;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 
+import com.javafun.timetracking.model.User;
 import com.javafun.timetracking.ui.ApplicationWorkbenchAdvisor;
 
 /**
- * This class controls all aspects of the application's execution
- * and is contributed through the plugin.xml.
+ * This class controls all aspects of the application's execution and is
+ * contributed through the plugin.xml.
  */
 public class Application implements IEntryPoint {
 
-    public int createUI() {
-        Display display = PlatformUI.createDisplay();
-        WorkbenchAdvisor advisor = new ApplicationWorkbenchAdvisor();
-        return PlatformUI.createAndRunWorkbench(display, advisor);
-    }
+	public int createUI() {
+		// String configName = "KeyStore";
+		// String configName = "UNIX";
+		User user = (User) RWT.getSessionStore().getHttpSession()
+				.getAttribute("user");
+		if (user == null) {
+			if (Status.CANCEL_STATUS == LoginManager.login(0)) {
+				IStatus status = new Status(IStatus.ERROR,
+						"com.javafun.timetracking", "Login failed", null);
+				ErrorDialog.openError(null, "Error",
+						"Login failed. Contact with administrator", status);
+				return -1;
+			}
+		}
+		Display display = PlatformUI.createDisplay();
+		WorkbenchAdvisor advisor = new ApplicationWorkbenchAdvisor();
+		RWT.getSessionStore().getHttpSession().setMaxInactiveInterval(900);
+		display.disposeExec(new Runnable() {
+			public void run() {
+				// Perform cleanup
+				// RWT.getSessionStore().removeAttribute("user");
+			}
+		});
+		return PlatformUI.createAndRunWorkbench(display, advisor);
+	}
+
 }
--- com.javafun.timetracking/src/com/javafun/timetracking/entrypoints/Settings.java
+++ com.javafun.timetracking/src/com/javafun/timetracking/entrypoints/Settings.java
@@ -1,32 +1,47 @@
-/**
- * 
- */
 package com.javafun.timetracking.entrypoints;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.rwt.RWT;
 import org.eclipse.rwt.lifecycle.IEntryPoint;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 
+import com.javafun.timetracking.model.User;
 import com.javafun.timetracking.ui.SettingsWorkbenchAdvisor;
 
 /**
  * @author Alexander Kunin
- *
+ * 
  */
 public class Settings implements IEntryPoint {
 
-    public Settings() {
-    }
+	public int createUI() {
+		// String configName = "KeyStore";
+		// String configName = "UNIX";
+		User user = (User) RWT.getSessionStore().getHttpSession()
+				.getAttribute("user");
+		if (user == null) {
+			if (Status.CANCEL_STATUS == LoginManager.login(0)) {
+				IStatus status = new Status(IStatus.ERROR,
+						"com.javafun.timetracking", "Login failed", null);
+				ErrorDialog.openError(null, "Error",
+						"Login failed. Contact with administrator", status);
+				return -1;
+			}
+		}
+		Display display = PlatformUI.createDisplay();
+		WorkbenchAdvisor advisor = new SettingsWorkbenchAdvisor();
+		RWT.getSessionStore().getHttpSession().setMaxInactiveInterval(900);
+		display.disposeExec(new Runnable() {
+			public void run() {
+				// Perform cleanup
+				// RWT.getSessionStore().removeAttribute("user");
+			}
+		});
+		return PlatformUI.createAndRunWorkbench(display, advisor);
 
-    /* (non-Javadoc)
-     * @see org.eclipse.rwt.lifecycle.IEntryPoint#createUI()
-     */
-    @Override
-    public int createUI() {
-        Display display = PlatformUI.createDisplay();
-        WorkbenchAdvisor advisor = new SettingsWorkbenchAdvisor();
-        return PlatformUI.createAndRunWorkbench(display, advisor);
-    }
-
+	}
 }
